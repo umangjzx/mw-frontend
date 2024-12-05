97f4e1bc76cd3eb9cd87a8ca809363ccb474b934
Rithik-02 <rithirithi246863@gmail.com>
2024-12-05 18:23:34 +0530

    completed notification card

--- src/components/common/button/button.d.ts
+++ src/components/common/button/button.d.ts
@@ -2,6 +2,6 @@ import { ButtonProps as AntButtonProps } from "antd";
 
 type ButtonProps = AntButtonProps & {
     customClassName?: string;
-    btnVariant?: "primary" | "secondary";
+    btnVariant?: "primary" | "secondary" | "error" | "success" | "link";
     title?: string;
 };
