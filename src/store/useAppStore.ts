d80761777e729dd15922535a8fc19a4b77c121e7
Rithik-02 <rithirithi246863@gmail.com>
2024-12-16 19:38:29 +0530

    splited store for componenet

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -3,22 +3,28 @@ import { create } from "zustand";
 import { useVolunteer } from "./useVolunteer";
 import { useLearner } from "./useLearner";
 import { useGlobalStore } from "./useGlobalStore";
+import { useComponentStore } from "./useComponenetStore";
 import { createJSONStorage, devtools, persist } from "zustand/middleware";
 
 export const useAppStore = create<UseAppStoreProps>()(
-    devtools(
-        persist(
-            (set, get, api) => {
-                return {
-                    ...useVolunteer(set, get, api), //* Add stores which are needed
-                    ...useLearner(set, get, api),
-                    ...useGlobalStore(set, get, api),
-                };
-            },
+    devtools((set, get, api) => {
+        const persistedState = persist<
+            Omit<UseAppStoreProps, keyof ReturnType<typeof useComponentStore>>
+        >(
+            (set, get, api) => ({
+                ...useVolunteer(set, get, api),
+                ...useLearner(set, get, api),
+                ...useGlobalStore(set, get, api),
+            }),
             {
                 name: "melody-wings-store",
                 storage: createJSONStorage(() => localStorage),
             }
-        )
-    )
+        )(set, get, api);
+
+        return {
+            ...persistedState,
+            ...useComponentStore(set, get, api),
+        };
+    })
 );
