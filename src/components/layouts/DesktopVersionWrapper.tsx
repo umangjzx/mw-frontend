4dd74656b301324a9e8f375fcd64554795c012d6
Rithik-02 <rithirithi246863@gmail.com>
2025-01-07 22:20:39 +0530

    feat: improve desktop version display logic based on viewport width

--- src/components/layouts/DesktopVersionWrapper.tsx
+++ src/components/layouts/DesktopVersionWrapper.tsx
@@ -3,24 +3,21 @@
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import DesktopVersionScreen from "@/components/common/DesktopVersionScreen";
+import useInnerWidth from "@/hooks/useInnerWidth";
 
 export default function DesktopVersionWrapper({ children }: { children: React.ReactNode }) {
     const [showDesktopVersionScreen, setShowDesktopVersionScreen] = useState(false);
     const routes = ["/login", "/onboarding"];
     const pathname = usePathname();
+    const innerWidth = useInnerWidth();
 
     useEffect(() => {
-        if (!routes.includes(pathname)) {
+        if (!routes.includes(pathname) && innerWidth < 768) {
             setShowDesktopVersionScreen(true);
         } else {
             setShowDesktopVersionScreen(false);
         }
-    }, [pathname]);
+    }, [pathname, innerWidth]);
 
-    return (
-        <>
-            {showDesktopVersionScreen && <DesktopVersionScreen />}
-            {children}
-        </>
-    );
+    return <>{showDesktopVersionScreen ? <DesktopVersionScreen /> : children}</>;
 }
