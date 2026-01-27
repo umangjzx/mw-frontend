d543533720e06749dedad9d03190419d049ebeae
Frexy-AI <rithirithi246863@gmail.com>
2026-01-27 12:22:59 +0530

    Refactor routing logic to redirect users to the home page instead of the login page. Update robots.txt to allow access to the root path and adjust sitemap URL. Remove deprecated login page component and enhance landing page structure with new components for improved user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,82 @@
-import LoginPage from "@/components/auth/Login";
-import TimeAgo from "javascript-time-ago";
-import en from "javascript-time-ago/locale/en";
-import ru from "javascript-time-ago/locale/ru";
-import DesktopVersionScreen from "@/components/common/DesktopVersionScreen";
-import { useEffect } from "react";
-import { useState } from "react";
-import { usePathname } from "next/navigation";
+"use client";
 
-TimeAgo.addDefaultLocale(en);
-TimeAgo.addLocale(ru);
+import Celebrate from "@/components/landingpage/Celebrate";
+import Community from "@/components/landingpage/Community";
+import Testimonials from "@/components/landingpage/testimonials";
+import ForLearner from "@/components/landingpage/ForLearner";
+import ForVolunteer from "@/components/landingpage/ForVolunteer";
+import Hero from "@/components/landingpage/Hero";
+import Impact from "@/components/landingpage/Impact";
+import WhyWeBuild from "@/components/landingpage/WhyWeBuild";
+import Footer from "@/components/onboarding/Footer";
+import { useQueryState } from "nuqs";
+import { useEffect, useState } from "react";
+import SkillsToLearn from "@/components/landingpage/SkillsToLearn";
 
 export default function Page() {
+    const [_, setParamMode] = useQueryState("signup_as");
+
+    useEffect(() => {
+        setParamMode(null);
+
+        let sr: any;
+        const setupScrollReveal = async () => {
+            if (typeof window !== "undefined") {
+                const ScrollReveal = (await import("scrollreveal")).default;
+                sr = ScrollReveal({
+                    origin: "bottom",
+                    distance: "3px",
+                    duration: 800,
+                    delay: 100,
+                    easing: "ease-out",
+                    reset: true,
+                    viewFactor: 0.1,
+                    viewOffset: { top: 0, right: 0, bottom: -100, left: 0 },
+                });
+
+                sr.reveal(".reveal", { interval: 100 });
+            }
+        };
+
+        setupScrollReveal();
+
+        return () => {
+            if (sr) sr.destroy();
+        };
+    }, []);
+
     return (
-        <div className="w-screen bg-background-input mx-auto p-6">
-            <LoginPage />
+        <div className="w-full overflow-x-hidden bg-background-input relative">
+            <Hero />
+            <div className="flex flex-col gap-20 lg:gap-[7rem] py-[7rem]">
+                <div className="reveal px-[9%]" id="about-us">
+                    <WhyWeBuild />
+                </div>
+                <div className="reveal px-[9%]" id="our-impact">
+                    <Impact />
+                </div>
+                <div className="flex flex-col lg:gap-20">
+                    <div className="reveal lg:px-[9%]">
+                        <ForLearner handleSignUp={() => setParamMode("learner")} />
+                    </div>
+                    <div className="reveal lg:px-[9%]">
+                        <ForVolunteer handleSignUp={() => setParamMode("volunteer")} />
+                    </div>
+                </div>
+                <div className="reveal px-[9%]">
+                    <SkillsToLearn />
+                </div>
+                <div className="reveal px-[7%]">
+                    <Community />
+                </div>
+                <div className="reveal">
+                    <Testimonials />
+                </div>
+                <div className="reveal px-[4%] lg:px-[9%]">
+                    <Celebrate />
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 }
